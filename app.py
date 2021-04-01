4a8cf33958d3e5669b6d6b911031f3737aa53ca2
abenikeb <59362713+abenikeb@users.noreply.github.com>
2021-04-02 00:48:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,15 @@ def hello_world():
 def predict():
     int_features = [float(x) for x in request.form.values()]
     final = [np.array(int_features)]
+    print(int_features)
+    print(final)
+    test = np.array([41.2, 11.5, 1, 11.2, 23.5, 45, 10, 4, 23])
+    test = test.reshape(1, -1)
     prediction = model.predict(final)
    
     if 5 > (0.5):
-       return render_template('index.html', pred='Probability occurance {}'.format(prediction), bhai="kuch karna hain iska ab?")
-#         return ("hellow abeni")
+#        return render_template('index.html', pred='Probability occurance {}'.format(prediction), bhai="kuch karna hain iska ab?")
+       return render_template('index.html', pred=1)
     else:
         return render_template('index.php', pred='Probability of fire occuring is {}'.format(prediction), bhai="Your Forest is Safe for now")
 
