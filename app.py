0dfdd1f4f97a6565075034e68cbdd121de7d1bda
abenikeb <59362713+abenikeb@users.noreply.github.com>
2021-04-02 01:56:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def predict():
     prediction = model.predict(final)
    
     if 5 > (0.5):
-       return render_template('index.php', pred='Probability occurance {}'.format(prediction), bhai="kuch karna hain iska ab?")
+       return render_template('index.php#prediction', pred='Probability occurance {}'.format(prediction), bhai="kuch karna hain iska ab?")
 #        return render_template('index.html', pred=1)
     else:
         return render_template('index.php', pred='Probability of fire occuring is {}'.format(prediction), bhai="Your Forest is Safe for now")
