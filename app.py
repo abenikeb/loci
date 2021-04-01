cd854aa484db69786d866370e46c42c4a72712c2
abenikeb <59362713+abenikeb@users.noreply.github.com>
2021-04-02 01:03:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ("hellow abeni---")
+    return ("hellow abeni-")
 
 
 @app.route('/predict_master', methods=['POST'])
@@ -20,7 +20,7 @@ def predict():
     print(final)
     test = np.array([41.2, 11.5, 1, 11.2, 23.5, 45, 10, 4, 23])
     test = test.reshape(1, -1)
-    prediction = model.predict_proba(final)
+    prediction = model.predict(test)
    
     if 5 > (0.5):
 #        return render_template('index.html', pred='Probability occurance {}'.format(prediction), bhai="kuch karna hain iska ab?")
