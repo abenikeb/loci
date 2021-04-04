b764c06dad4a84ff480d87c8c75397f0a943ebde
abenikeb <59362713+abenikeb@users.noreply.github.com>
2021-04-04 21:15:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ("hellow abeni-")
+    return ("Hello! This is loci company")
 
 
 @app.route('/predict_master', methods=['POST'])
