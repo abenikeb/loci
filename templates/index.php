d2768ee6c1d0fe2f94a8c4a2a59d254d615ae58b
abenikeb <59362713+abenikeb@users.noreply.github.com>
2021-04-04 21:20:58 +0300

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -21,15 +21,27 @@
     <meta name="author" content="">
     <link rel="icon" href="../assets/img/loci.png">
 
-    <title>Loci</title>
+    <title>prediction</title>
 
     <link href="../static/css/bootstrap.min.css" rel="stylesheet">
-
     <link href="../static/css/carousel.css" rel="stylesheet">
     <link href="form-validation.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <script src="https://kit.fontawesome.com/f611d4bc46.js" crossorigin="anonymous"></script>
     
-  </head>
+  </head>  
   <body>
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        #map {
+            height: 100%;
+        }
+    </style>
+    
 
     <header>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
@@ -43,174 +55,71 @@
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#about_as">About as</a>
+              <a class="nav-link" href="#about_as"><i class="fa fa-user"></i> ABOUT </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#prediction">Prediction</a>
+              <a class="nav-link" href="#prediction"><i class="fa fa-share"></i> Prediction</a>
             </li>
             <li class="nav-item">
               <a class="nav-link disabled" href="#">Disabled</a>
             </li>
           </ul>
-          <form class="form-inline mt-2 mt-md-0">
-            <!-- <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"> -->
-            <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button> -->
-              <a href="../login" class="btn btn-outline-secondary my-2 my-sm-0">
-                 Logout
-              </a>
-          </form>
+         
         </div>
       </nav>
     </header>
     <main role="main">
+<br ><br ><br >
 
-      <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="first-slide" src="../static/img/locust_1280p.jpg" alt="First slide">
-            <div class="container">
-              <div class="carousel-caption text-left">
-                <h1>Loci</h1>
-                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p><a class="btn btn-lg btn-info" href="#" role="button">Sign up today</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="second-slide" src="../static/img/lll.jpg" alt="Second slide">
-            <div class="container">
-              <div class="carousel-caption">
-                <h1>Loci</h1>
-                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p><a class="btn btn-lg btn-info" href="#" role="button">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="third-slide" src="../static/img/pexels-erik-karits-5230651--.jpg" alt="Third slide">
-            <div class="container">
-              <div class="carousel-caption text-right">
-                <h1>Loci</h1>
-                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p><a class="btn btn-lg btn-info" href="#" role="button">Browse gallery</a></p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-        <div id="about_as"></div>
-      </div>
-     
+      <div class="container marketing" id="prediction">
 
-
-      <div class="container marketing">
-
-        <!-- Three columns of text below the carousel -->
-        <div class="row" >
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="../static/img/pexels-terje-sollie-336948.jpg" alt="Generic placeholder image" width="140" height="140">
-            <h2>Prediction</h2>
-            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="../static/img/hitmap.jpg" alt="Generic placeholder image" width="140" height="140">
-            <h2>Hit Map</h2>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="../static/img/pexels-erik-karits-5230651.jpg" alt="Generic placeholder image" width="140" height="140">
-            <h2>Alert</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-        </div><!-- /.row -->
-
-
-        <!-- START THE FEATURETTES -->
-
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" src="../static/img/hh.png" alt="Generic placeholder image">
-          </div>
-        </div>
-
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5 order-md-1">
-            <img class="featurette-image img-fluid mx-auto" src="../static/img/map.png" alt="Generic placeholder image">
-          </div>
-        </div>
-
-        <hr class="featurette-divider" id="prediction">
+       
+        <br >
 
          <div class="row">
         <div class="col-md-4 order-md-2 mb-4">
           <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-muted">Result</span>
-            <span class="badge badge-info badge-pill">{{pred}}</span>
+            <span class="text-muted"><i class="fa fa-user"></i> Info Desk</span>
+            <span class="badge badge-info badge-pill"></span>
           </h4>
           <ul class="list-group mb-3">
+             <li class="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 class="my-0">Model Accurcy</h6>
+                <small class="text-muted"></small>
+              </div>
+              <span class="text-muted">87%</span>
+            </li>
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
                 <h6 class="my-0">Current Loc:</h6>
-                <small class="text-muted">Occurance Option</small>
+                <small class="text-muted"><?php echo $current->city; ?>, <?php echo $current->country; ?></small>
               </div>
-              <span class="text-muted">89%</span>
+              <span class="text-muted"><?php echo $current->state; ?></span>
             </li>
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 class="my-0">Longitutde</h6>
-                 <?php if ($status == 0): ?>
-                <small class="text-muted"></small>
-                <?php else: ?>
-                <small class="text-muted"></small>
-                <?php endif; ?>
+                <h6 class="my-0">Location</h6>
+                  <small class="text-muted"><?php echo $current->location; ?></small>
+             
               </div>
-              <span class="text-muted">11%</span>
+              <span class="text-muted"></span>
             </li>
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 class="my-0">Latitude</h6>
+                <h6 class="my-0">Latitude & Longitude</h6>
                 <small class="text-muted">Brief description</small>
               </div>
-              <span class="text-muted">$5</span>
+              <span class="text-muted"></span>
             </li>
             <li class="list-group-item d-flex justify-content-between bg-light">
               <div class="text-success">
                 <h6 class="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
+                <small>Total Data</small>
               </div>
-              <span class="text-success">-$5</span>
+              <span class="text-success">-</span>
             </li>
-            <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
-            </li>
+           
           </ul>
 
          
@@ -225,54 +134,41 @@
           </form>
         </div>
         <div class="col-md-8 order-md-1">
-          <button class="btn btn-info" class="mb-3"><- Back </button>
-          
+          <a href="../index.php" class="btn btn-info" class="mb-3"><i class="fa fa-arrow-circle-o-left"></i> Back </a>
+        
+          <hr class="mb-4">
             <br>
-             <h4 class="mb-3">Generate Hitmap</h4>
-            <p>For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.<p>
-
-
-            <div class="custom-control custom-checkbox">
+            <h4 class="mb-3"><b>Prediction Result</b></h4>
+            
+             <!-- <div class="custom-control custom-checkbox">
               <input type="checkbox" class="custom-control-input" id="same-address">
-              <label class="custom-control-label" for="same-address">Addis Abeba</label>
-            </div>
-            <div class="custom-control custom-checkbox">
-              <input type="checkbox" class="custom-control-input" id="save-info">
-              <label class="custom-control-label" for="save-info">Jiamma</label>
-            </div>
-              <div class="custom-control custom-checkbox">
-              <input type="checkbox" class="custom-control-input" id="same-address">
-              <label class="custom-control-label" for="same-address">Mekele</label>
-            </div>
-            <div class="custom-control custom-checkbox">
-              <input type="checkbox" class="custom-control-input" id="save-info">
-              <label class="custom-control-label" for="save-info">Hawasa</label>
-            </div>
-            <hr class="mb-4">
-
+              <label class="custom-control-label" for="same-address">Current Month</label>
+            </div> -->
+            <div class="custom-control custom-checkbox">            
+            
  
-            <button class="btn btn-danger" type="submit">Generate Hit</button>
-          
-        </div>
-      </div>
+            <a href="hitmap.php" class="btn btn-danger" class="btn btn-info btn-lg">{{pred}}</a>
+            <hr class="mb-4">
+             <p><em>The result shows the area may be..!<em></p>
+             <p>The development of automated tools will help the organization and the 
+               <em>government to early detect and predict locust swarm suitable locations based on<em> 
+                 the<b> metrological </b>and other seasonal factors such as wind direction and month of the year. If the invasion of the locust is early predicted it will help for taking corrective measures and to plan early the resources needed to combat the locust spread</p>
+
+      
 
         <hr class="featurette-divider">
-
-        <!-- /END THE FEATURETTES -->
-
       </div><!-- /.container -->
-
+    
+      <div class="panel-group">
+ <br><br><br><br><br><br><br>
 
       <!-- FOOTER -->
       <footer class="container">
         <p class="float-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        <p>&copy; 2021 Loci Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
       </footer>
     </main>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
     <script src="../assets/js/vendor/popper.min.js"></script>
@@ -301,8 +197,8 @@
         }, false);
       })();
     </script>
+   
+  
+
   </body>
 </html>
-
-
-
