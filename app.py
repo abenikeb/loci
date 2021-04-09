994c82b2886b3209e0f0e66babf8920f51502822
abenikeb <59362713+abenikeb@users.noreply.github.com>
2021-04-09 13:43:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ("Hello! This is loci company")
+    return ("Hello! This is loci company-")
 
 
 @app.route('/predict_master', methods=['POST'])
